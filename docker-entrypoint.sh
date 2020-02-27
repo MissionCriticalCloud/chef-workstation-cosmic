8439a052b8da7bf073c36a1ea60ad61bcb8ef5d5
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-02-27 23:23:48 +0100

    Add ShellCheck
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,24 +7,27 @@ chef -v && echo
 
 # Load git private key if one is provided
 if [ -n "${GIT_PRIVATE_KEY}" ]; then
-    mkdir -p ~/.ssh && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+    mkdir -p ~/.ssh && echo -e "Host *\\n\\tStrictHostKeyChecking no\\n" > ~/.ssh/config
 
-    echo "Setting up SSH Agent... "
+    echo "Setting up SSH Agent..."
     eval "$(ssh-agent -s)" > /dev/null
-    echo "Adding private key... "
-    ssh-add <(echo "${GIT_PRIVATE_KEY}") 2&>1 /dev/null
+    echo "Adding private key..."
+    ssh-add <(echo "${GIT_PRIVATE_KEY}")
+    echo
 fi
 
 # Fetch chef config repository if one is provided
 if [ -n "${CHEF_REPO}" ] && [ -n "${GIT_PRIVATE_KEY}" ]; then
-    echo "Fetching Chef config from ${CHEF_REPO}... "
+    echo "Fetching Chef config from ${CHEF_REPO}..."
     git clone -q "${CHEF_REPO}" ~/.chef
+    echo
 fi
 
 # Add a Chef user PEM if one is provided
 if [ -n "${CHEF_PEM}" ] && [ -n "${CHEF_REPO}" ] && [ -n "${CHEF_USER}" ]; then
-    echo "Writing Chef PEM for ${CHEF_REPO}... "
+    echo "Writing Chef PEM for ${CHEF_USER}..."
     echo "${CHEF_PEM}" > "${HOME}/.chef/${CHEF_USER}.pem"
+    echo
 fi
 
 exec "$@"
